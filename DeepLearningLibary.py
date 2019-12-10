7f40cda617ea41dac49ab7141648aed09cfb0ac1
LuposX <LuposX@users.noreply.github.com>
2019-12-10 21:23:30 +0100

    bias backprop

--- DeepLearningLibary.py
+++ DeepLearningLibary.py
@@ -69,6 +69,8 @@ class NeuralNetwork:
         # for visuliozing
         self.x_train_loss_history = []
         self.y_train_loss_history = []
+
+        self.bias_weight_tmp = []
         
     def add_bias(self, x) -> List[float]:
         x = np.array([np.insert(x, 0, 1)])
@@ -364,13 +366,21 @@ class NeuralNetwork:
                     d_J = self.loss_derivative(y=target, y_hat=self.output_model)
                     error_term = np.array([np.multiply(d_a.flatten(), d_J.flatten())])
                     self.error_term_cache.append(error_term)
+
+                    tmp_matrix_weight = np.asarray(self.weights[idx - 1])
+                    tmp_bias_weight_t = np.array(tmp_matrix_weight.T[0])
+                    self.bias_weight_tmp.append([tmp_bias_weight_t])
                 else:
                     temp_idx = "z" + str(idx)
                     layer_cache_tmp_drop_bias = np.delete(self.layer_cache[temp_idx], 0, 1)
+
                     d_a = self.activation_derivative(layer, layer_cache_tmp_drop_bias)
 
                     d_J = 0
                     for item in reversed(self.error_term_cache):
+                        tmp_matrix_weight = np.asarray(self.weights[idx - 1])
+                        self.bias_weight_tmp.append([tmp_matrix_weight.T[0]])
+
                         weights_tmp_drop_bias = np.delete(self.weights[idx], 0, 1)
                         d_J = d_J + np.dot(weights_tmp_drop_bias.T, item.T)
 
@@ -385,6 +395,7 @@ class NeuralNetwork:
                 weight_change = err_temp * cache_tmp
                 self.weight_change_cache.append(weight_change)
 
+        # update weights
         for idx in range(0, len(self.weight_change_cache)):  # reversed because we go backwards
             curr_weight = self.weights[-idx - 1]
             curr_weight = np.delete(curr_weight, 0, 1)  # delete bias
@@ -394,8 +405,16 @@ class NeuralNetwork:
             curr_weight = curr_weight - total_weight_change
             self.weights[-idx - 1] = curr_weight
 
+        # update bias
+        if layer["layer_type"] == "output_layer":
+            for i in range(0, len(self.bias_weight_tmp)):
+                tmp_weight_bias = np.asarray(self.bias_weight_tmp[i])
+                tmp_error_term_bias = np.asarray(self.error_term_cache[i])
+                self.bias_weight_tmp[i] = tmp_weight_bias - (self.alpha * tmp_error_term_bias)
+
+        # insert bias in weights
         for i in range(0, len(self.weights)):
-            self.weights[i] = np.insert(self.weights[i], obj=0, values=0.5, axis=1)  # insert the weights for the biases
+            self.weights[i] = np.insert(self.weights[i], obj=0, values=self.bias_weight_tmp[i], axis=1)  # insert the weights for the biases
 
     # TODO: "train" is work in progress
     def train(self, how_often, epochs=20) -> None:
@@ -465,18 +484,18 @@ class NeuralNetwork:
 
 if __name__ == "__main__":
     # data for nn and target
-    x = np.array([[0.05, 0.10]], dtype=float)
-    y = np.array([[0.5, 0.2]], dtype=float)
+    x = np.array([[1, 0]], dtype=float)
+    y = np.array([[0, 1]], dtype=float)
 
     # nn_architecture is WITH input-layer and output-layer
     nn_architecture = [{"layer_type": "input_layer", "layer_size": 2, "activation_function": "none"},
                        {"layer_type": "hidden_layer", "layer_size": 2, "activation_function": "sigmoid"},
                        {"layer_type": "output_layer", "layer_size": 2, "activation_function": "sigmoid"}]
 
-    weights_data = [np.array([[0.5, 0.15, 0.2], [0.5, 0.25, 0.3]], dtype=float), np.array([[0.6, 0.4, 0.45], [0.6, 0.5, 0.55]], dtype=float)]
+    weights_data = [np.array([[2, 0.15, 0.2], [2, 0.25, 0.3]], dtype=float), np.array([[4, 0.4, 0.45], [4, 0.5, 0.55]], dtype=float)]
     weights_data = weights_data
 
     #, custom_weights=True, custom_weights_data=weights_data
-    NeuralNetwork_Inst = NeuralNetwork(x, y, nn_architecture, 0.5, 5, custom_weights=True, custom_weights_data=weights_data)
-    NeuralNetwork_Inst.train(how_often=100, epochs=500)
+    NeuralNetwork_Inst = NeuralNetwork(x, y, nn_architecture, 0.1, 5, custom_weights=True, custom_weights_data=weights_data)
+    NeuralNetwork_Inst.train(how_often=10, epochs=400)
     NeuralNetwork_Inst.visulize()
